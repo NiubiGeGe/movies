59e9dd46579d2436d1bf1333c8d2a93f6cdfb767
Zheng Wang <wz62111126@gmail.com>
2015-08-24 19:42:46 -0700

    reformatted

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,28 +1,65 @@
+"""
+    This python file a supporting file for fresh_tomatoes.py.
+    It define multiple instances of the movie then store them in a list.
+    ''fresh_tomatoes.open_movies_page'' function uses the list to generate a
+    movie website.
+
+    example:
+    movie = media.Movie(
+        "Title", "Duration_of_the_movie","Main_actors", "Youtube_URL",
+        "Storyline", "Poster_image_URL
+    )
+"""
+
 import media
 import fresh_tomatoes
 
 
-toy_story = media.Movie("Toy Story", 81, "Joss Whedon, Andrew Stanton", "https://www.youtube.com/watch?v=4KPTXpQehio",  "A story of toys", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg")
+toy_story = media.Movie(
+    "Toy Story", 81, "Joss Whedon, Andrew Stanton",
+    "https://www.youtube.com/watch?v=4KPTXpQehio",
+    "A story of toys",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
+)
+
+avatar = media.Movie(
+    "Avatar", 161,
+    "Sam Worthington, Zoe Saldana, Stephen Lang",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+    "Aliean movie",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg"
+)
+
+school_of_rock = media.Movie(
+    "School of Rock", 105, "Jack Black, Joan Cusack",
+    "https://www.youtube.com/watch?v=XCwy6lW5Ixc",
+    "Using rock music to learn",
+    "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg"
+)
+
+ratatouille = media.Movie(
+    "Ratatouille", 111, "Patton Oswalt, Ian Holm, Lou Romano",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk",
+    "Arat is a chef in Paris",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg"
+)
+
+midnight_in_paris = media.Movie(
+    "Midnight in Paris", 94, "Kathy Bates, Adrien Brody, Carla Bruni",
+    "https://www.youtube.com/watch?v=FAfR8omt-CY", "Goint back in time",
+    "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg"  # noqa
+)
 
 
-avatar = media.Movie("Avatar", 161, "Sam Worthington, Zoe Saldana, Stephen Lang", "https://www.youtube.com/watch?v=5PSNL1qE6VY", "Aliean movie", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg")
+hunger_games = media.Movie(
+    "Hunger Games", 142, "Jennifer Lawrence, Josh Hutcherson",
+    "https://www.youtube.com/watch?v=SMGRhAEn6K0", "A Good movie",
+    "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg"
+)
 
-
-school_of_rock = media.Movie("School of Rock", 105, "Jack Black, Joan Cusack", "https://www.youtube.com/watch?v=XCwy6lW5Ixc", "Using rock music to learn", "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg")
-
-
-ratatouille = media.Movie("Ratatouille", 111, "Patton Oswalt, Ian Holm, Lou Romano", "https://www.youtube.com/watch?v=c3sBBRxDAqk", "Arat is a chef in Paris", "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg")
-
-
-midnight_in_paris = media.Movie("Midnight in Paris", 94, "Kathy Bates, Adrien Brody, Carla Bruni", "https://www.youtube.com/watch?v=FAfR8omt-CY", "Goint back in time", "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg")
-
-
-hunger_games = media.Movie("Hunger Games", 142, "Jennifer Lawrence, Josh Hutcherson", "https://www.youtube.com/watch?v=SMGRhAEn6K0", "A Good movie", "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg")
-
-
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-
+movies = [
+    toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris,
+    hunger_games
+]
 
 fresh_tomatoes.open_movies_page(movies)
-
-
--- media.py
+++ media.py
@@ -1,24 +1,25 @@
 import webbrowser
 
 
-
 class Movie():
-	""" This is a Movie class contains title, duration, actor, trailer_url, movie_storyline, poster_image"""
+    """This is a Movie class contains title, duration, actor, trailer_url,
+    movie_storyline, poster_image.
+    """
 
+    def __init__(self, movie_title, duration, actor, trailer_youtube,
+                 movie_storyline, poster_image):
+        # Constructor of Movie class
+        self.title = movie_title
+        self.duration = duration
+        self.actor = actor
+        self.trailer_youtube_url = trailer_youtube
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
 
-	def __init__(self, movie_title, duration, actor, trailer_youtube, movie_storyline, poster_image):
-		#Constructor of Movie class
-		self.title = movie_title
-		self.duration = duration
-		self.actor = actor
-		self.trailer_youtube_url = trailer_youtube
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
+    def show_trailer(self):
+        # open trailer in webbrowser
+        webbrowser.open(self.trailer_youtube_url)
 
-	def show_trailer(self):
-		#open trailer in webbrowser
-		webbrowser.open(self.trailer_youtube_url)
-
-	def show_actor(self):
-		#print main actors name
-		print self.actor
+    def show_actor(self):
+        # print main actors name
+        print self.actor
